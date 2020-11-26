2a1caec0a4d037eb10fba3f03c5dec4f09b8b78b
Adrian D. Moseley <adrian.moseley@gmail.com>
2020-11-25 19:20:04 -0600

    Update restore script

--- restore_apps.sh
+++ restore_apps.sh
@@ -6,73 +6,80 @@
 ##########      twitter: @adrianmoseley
 ###################################################
 ###################################################
-
-Other configuration items not handled by script:
-
-- How to Change the GDM Background on Ubuntu
-    wget github.com/thiggy01/ubuntu-20.04-change-gdm-background/raw/master/ubuntu-20.04-change-gdm-background
-
-- AppImageLauncher
-    https://github.com/TheAssassin/AppImageLauncher/releases
-
-- AppImage Apps    
-    balenaEtcher
-    ClipGrab
-    OpenShot
-    RawTherapee
-    okular
-    flightlight
-
-- Horizon View Client
-    https://www.vmware.com/go/viewclients
-
-- Configure and Install RetroPi Game emulator
-    https://markontech.com/linux/how-to-install-retropie-on-ubuntu-tutorial/
-
-- Configure Xbox One S Controller
-    https://www.youtube.com/watch?v=bAI4vnlQhPg
-    sudo apt install sysfsutils
-    sudo nano /etc/sysfs.conf
-    place the following at the end of the file. "/module/bluetooth/parameters/disable_ertm=1"
-
-- Themes
-    https://itsubuntu.com/11-best-ubuntu-20-04-lts-themes-in-2020/
-
-- Exodus.io
-    https://www.exodus.io/
-
-- Daedalus
-    https://daedaluswallet.io/
-
-- VScode
-    https://code.visualstudio.com/Download
+##	Other configuration items not handled by script:
+##	
+##	- How to Change the GDM Background on Ubuntu
+##	    wget github.com/thiggy01/ubuntu-20.04-change-gdm-background/raw/master/ubuntu-20.04-change-gdm-background
+##	
+##	- AppImageLauncher
+##	    https://github.com/TheAssassin/AppImageLauncher/releases
+##	
+##	- AppImage Apps    
+##	    balenaEtcher
+##	    ClipGrab
+##	    OpenShot
+##	    RawTherapee
+##	    okular
+##	    flightlight
+##	
+##	- Horizon View Client
+##	    https://www.vmware.com/go/viewclients
+##	
+##	- Configure and Install RetroPi Game emulator
+##	    https://markontech.com/linux/how-to-install-retropie-on-ubuntu-tutorial/
+##	
+##	- Configure Xbox One S Controller
+##	    https://www.youtube.com/watch?v=bAI4vnlQhPg
+##	    sudo apt install sysfsutils
+##	    sudo nano /etc/sysfs.conf
+##	    place the following at the end of the file. "/module/bluetooth/parameters/disable_ertm=1"
+##	
+##	- Themes
+##	    https://itsubuntu.com/11-best-ubuntu-20-04-lts-themes-in-2020/
+##	
+##	- Exodus.io
+##	    https://www.exodus.io/
+##	
+##	- Daedalus
+##	    https://daedaluswallet.io/
+##	
+##	- VScode
+##	    https://code.visualstudio.com/Download
 ###################################################
 ###################################################
 
 #Add Ubuntu Repo
+echo "Adding the Ubuntu universe repo:"
 sudo add-apt-repository universe
+clear
 
 #add canocial partners
 
 #Add ODS Studio PPA
+echo "Adding the ODS Studio PPA Repo:"
 sudo add-apt-repository ppa:obsproject/obs-studio
+clear
 
 #AppImageLauncher to manage appimage Based Apps
 #https://github.com/TheAssassin/AppImageLauncher
+echo "Adding the AppImageLauncher repo"
 sudo add-apt-repository ppa:appimagelauncher-team/stable
+clear
 
 #Add ConkyManager not working
 #sudo add-apt-repository ppa:linuxmint-tr/araclar
 
 #Add Brave Browser Repo
+echo "Install curl and Brave Browser repo:"
+sudo apt install curl
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update && sudo apt upgrade -y
+clear
 
 #Install Apps
-sudo apt install brave-browser apt-transport-https gnupg synaptic htop screenfetch synapse flameshot stacer snap peek  obs-studio python3 python3-pip golang  celluloid speedcrunch conky-all xscreensaver xscreensaver-gl-extra xscreensaver-data-extra appimagelauncher openshot glances okular simple-scan unity-tweak-tool gdebi pavucontrol gimp gparted preload gnome-tweak-tool bleachbit pdfsam openssh-server sysfsutils curl -y
-
-
+echo "Install all favorite apps"
+sudo apt install brave-browser apt-transport-https gnupg synaptic htop screenfetch synapse flameshot stacer snap peek  obs-studio python3 python3-pip golang  celluloid speedcrunch conky-all xscreensaver xscreensaver-gl-extra xscreensaver-data-extra appimagelauncher openshot glances okular simple-scan unity-tweak-tool gdebi pavucontrol gimp gparted preload gnome-tweak-tool bleachbit pdfsam openssh-server sysfsutils appimagelauncher git -y
 
 
 #Gnome Extensions
